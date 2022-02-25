44e4bdb9190404835e3641b5a6334256d4b3987e
skim92 <sunny.kim@codesmithdev.com>
2022-02-25 14:40:15 -0500

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0d323c1b676db24d72d491ae5547c474171b349e',
+    'name' => 'consolibyte/quickbooks',
+  ),
+  'versions' => 
+  array (
+    'consolibyte/quickbooks' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0d323c1b676db24d72d491ae5547c474171b349e',
+    ),
+  ),
+);
